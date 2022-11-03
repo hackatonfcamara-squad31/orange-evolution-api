5a2e09c4e359e0d3c8e69e57ecd3819c6f4da81d
Leandro Gonçalves <leolucas9912@gmail.com>
2022-11-03 00:29:54 -0300

    fix: moving /me route to app.controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,19 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { CurrentUser } from './modules/auth/decorators/current-user.decorator';
+import { User } from './modules/users/entities/user.entity';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
   @Get()
   getPing(): string {
     return this.appService.getPing();
+  }
+
+  @Get('me')
+  getMe(@CurrentUser() user: User): User {
+    return user
   }
 }
--- src/modules/users/user.controller.ts
+++ src/modules/users/user.controller.ts
@@ -1,5 +1,4 @@
-import { Body, Controller, Post, Get } from '@nestjs/common';
-import { CurrentUser } from '../auth/decorators/current-user.decorator';
+import { Body, Controller, Post } from '@nestjs/common';
 import { CreateUserDTO } from './dtos/create-user.dto';
 import { User } from './entities/user.entity';
 import { UsersService } from './user.service';
@@ -11,10 +10,5 @@ export class UsersController {
   @Post()
   async create(@Body() userData: CreateUserDTO): Promise<User> {
     return this.usersService.create(userData);
-  }
-
-  @Get('me')
-  getMe(@CurrentUser() user: User): User {
-    return user
   }
 }
