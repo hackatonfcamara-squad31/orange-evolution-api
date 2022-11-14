d6e03d54359ca0de550a470dbe6400b50f252bc8
kevin-neves <kevinjrneves@gmail.com>
2022-11-14 12:06:58 -0300

    docs: adding insomnia content delete and reorder

--- src/modules/content/dto/reorder-content-dto.ts
+++ src/modules/content/dto/reorder-content-dto.ts
@@ -1,12 +1,16 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNumber, IsUUID } from 'class-validator';
 
 export class ReorderContentDTO {
+  @ApiProperty()
   @IsUUID()
   id: string;
 
+  @ApiProperty()
   @IsNumber({}, { message: 'Por favor, informe a posição do módulo.' })
   order: number;
 
+  @ApiProperty()
   @IsUUID()
   module_id: string;
 }
